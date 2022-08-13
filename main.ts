45733219dd4efa68b6f2042298ee3d8509ad65f3
NovaKwan <110724796+NovaKwan@users.noreply.github.com>
2022-08-13 15:29:44 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
 input.onSound(DetectedSound.Loud, function () {
-    basic.showNumber(randint(1, 3))
+    dothing()
+})
+function dothing () {
+    input.onSound(DetectedSound.Loud, function () {
+    })
+basic.showNumber(randint(1, 3))
     music.playTone(500, music.beat(BeatFraction.Half))
     basic.pause(3000)
     basic.clearScreen()
-})
+    input.onSound(DetectedSound.Loud, function () {
+        dothing()
+    })
+}
 input.setSoundThreshold(SoundThreshold.Loud, 158)
